59de7a3d3428247a821da70eb6fef38e74e2c8b3
Vika <vika.prozorova@gmail.com>
2015-07-18 02:28:04 +0300

    Add table users

--- server/db.sql
+++ server/db.sql
@@ -1,10 +1,22 @@
 drop table if exists notes;
+drop table if exists users;
+
 
 create table notes (
-    id        INT       NOT NULL AUTO_INCREMENT,
-    text      TEXT      NOT NULL,
-    date      DATETIME  NOT NULL,
-    isDeleted BOOLEAN   NOT NULL    DEFAULT FALSE,
+    id        INT         NOT NULL AUTO_INCREMENT,
+    text      TEXT        NOT NULL,
+    date      DATETIME    NOT NULL,
+    isDeleted BOOLEAN     NOT NULL DEFAULT FALSE,
+    user      VARCHAR(40) NOT NULL,
 
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (user) REFERENCES users (email)
+);
+
+create table users (
+    email        VARCHAR(40) NOT NULL,
+    password     VARCHAR(20) NOT NULL, --TODO
+    securityCode VARCHAR(50) NOT NULL,
+
+    PRIMARY KEY (email)
 );
