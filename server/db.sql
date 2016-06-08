d28252d96587b543879c3d5d09b66598abd99fef
Vika <vika.prozorova@gmail.com>
2016-06-09 01:22:49 +0300

    New db structure

--- server/db.sql
+++ server/db.sql
@@ -1,5 +1,7 @@
 drop table if exists notes;
 drop table if exists users;
+drop table if exists customers;
+drop table if exists teammates;
 
 create table users (
     id       INT          NOT NULL AUTO_INCREMENT,
@@ -10,7 +12,7 @@ create table users (
 
     PRIMARY KEY (id),
     UNIQUE KEY (email)
-);
+)  DEFAULT CHARACTER SET utf8;
 
 create table notes (
     id        INT         NOT NULL AUTO_INCREMENT,
@@ -18,7 +20,44 @@ create table notes (
     date      DATETIME    NOT NULL,
     isDeleted BOOLEAN     NOT NULL DEFAULT FALSE,
     user      INT         NOT NULL,
+    customer  INT         NOT NULL,
+    teammate  INT         NOT NULL,
 
     PRIMARY KEY (id),
     FOREIGN KEY (user) REFERENCES users (id)
-);
+)  DEFAULT CHARACTER SET utf8;
+
+create table customers (
+    id          INT          NOT NULL AUTO_INCREMENT,
+    parentID    INT,
+    name        VARCHAR(40)  NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    project     VARCHAR(40)  NOT NULL,
+    priority    ENUM('low', 'normal', 'high') NOT NULL DEFAULT 'normal',
+
+    PRIMARY KEY (id),
+    UNIQUE KEY (name),
+    FOREIGN KEY (project) REFERENCES projects (id)
+) DEFAULT CHARACTER SET utf8;
+
+create table teammates (
+    id          INT          NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(40)  NOT NULL,
+    project     VARCHAR(40)  NOT NULL,
+    priority    ENUM('low', 'normal', 'high') NOT NULL DEFAULT 'normal',
+
+    PRIMARY KEY (id),
+    UNIQUE KEY (name),
+    FOREIGN KEY (project) REFERENCES projects (id)
+) DEFAULT CHARACTER SET utf8;
+
+create table projects (
+    id          INT          NOT NULL AUTO_INCREMENT,
+    parentID    INT,
+    name        VARCHAR(40)  NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    priority    ENUM('low', 'normal', 'high') NOT NULL DEFAULT 'normal',
+
+    PRIMARY KEY (id),
+    UNIQUE KEY (name),
+) DEFAULT CHARACTER SET utf8;
